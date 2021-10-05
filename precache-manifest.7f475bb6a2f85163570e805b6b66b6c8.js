75278bc43c569d711b55c5f0cf70d67399c33fe4
CamilaDamasio <camiladamasioef@hotmail.com>
2021-10-04 21:54:43 -0300

    Updates

---
+++ precache-manifest.7f475bb6a2f85163570e805b6b66b6c8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded63b93c2a2cd1e6ce71380e3c81c18",
+    "url": "/recipes-app-project/index.html"
+  },
+  {
+    "revision": "e3259c35f61ed3442e94",
+    "url": "/recipes-app-project/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "1182e2c943e53bb53f26",
+    "url": "/recipes-app-project/static/css/main.565a6d98.chunk.css"
+  },
+  {
+    "revision": "e3259c35f61ed3442e94",
+    "url": "/recipes-app-project/static/js/2.684ed862.chunk.js"
+  },
+  {
+    "revision": "9353b4df7938eafd5f8e3a0cf5a68c12",
+    "url": "/recipes-app-project/static/js/2.684ed862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1182e2c943e53bb53f26",
+    "url": "/recipes-app-project/static/js/main.f024248f.chunk.js"
+  },
+  {
+    "revision": "d51b35780514241f3f86",
+    "url": "/recipes-app-project/static/js/runtime-main.f7ecbc99.js"
+  },
+  {
+    "revision": "ed0b0a6540484062fcfd41af5e6168de",
+    "url": "/recipes-app-project/static/media/KitchenHome-rg0gB.ed0b0a65.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/recipes-app-project/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipes-app-project/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/recipes-app-project/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/recipes-app-project/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/recipes-app-project/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/recipes-app-project/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/recipes-app-project/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes-app-project/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "66bea47c318fda89d67f1c84ba348018",
+    "url": "/recipes-app-project/static/media/tryberchef.66bea47c.jpg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipes-app-project/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
