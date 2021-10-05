5dc67de71810f0998992d851ab13191657cc038f
CamilaDamasio <camiladamasioef@hotmail.com>
2021-10-04 21:58:51 -0300

    altera index.js troca browserrouter por hashrouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -9,9 +9,9 @@ import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root'),
 );
