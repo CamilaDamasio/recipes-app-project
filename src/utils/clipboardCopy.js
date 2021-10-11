5e3c6947c8387210d24512414db8978e48ca82bd
CamilaDamasio <camiladamasioef@hotmail.com>
2021-10-11 10:21:46 -0300

    corrige codigo de link de compartilhamento

--- src/utils/clipboardCopy.js
+++ src/utils/clipboardCopy.js
@@ -1,7 +1,7 @@
 const copy = require('clipboard-copy');
 
 const clipboardCopy = (type, id) => {
-  copy(`${window.location.origin}/${type}s/${id}`).then(() => {
+  copy(`${window.location.origin}/recipes-app-project/#/${type}s/${id}`).then(() => {
     document.querySelector(`#copyMessage${id}`).innerHTML = 'Link copiado!';
   });
 };
